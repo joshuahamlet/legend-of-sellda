3c85844baaae8b398593b9054390c440cda86250
Joshua Hamlet <joshua7hamlet@gmail.com>
2020-08-26 05:17:12 -0700

    Improvements to CartView

--- src/Views/CartView.js
+++ src/Views/CartView.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion'
 import { removeFromCart, adjustQuantityCart } from '../Actions/cartAction'
+import rupee from '../rupee.png'
 import './CartView.css'
 
 
@@ -37,11 +38,12 @@ const CartView = (props) => {
         visible: {
             opacity: 1,
             x: '0vw',
-            transition: { delay: .1, duration: .75 }
+            transition: { delay: .1, duration: .75 },
+            transitionEnd: { finalized: 'yes' }
         },
         exit: {
             x: '100vw',
-            transition: { delay: .1, duration: .75 }
+            transition: { delay: .1, duration: .75 }            
         },
         hiddenB: {
             opacity: 0,
@@ -58,9 +60,12 @@ const CartView = (props) => {
         }
     }
 
+    const animationFinalized = containerVariants.visible.transitionEnd.finalized
+
+
     return(
 
-        <div className="cart-container">
+        <div className="cart-container" style={ animationFinalized ? {overflow: "hidden"} : ""}>
 
         <motion.div className="cart-list"
             variants={containerVariants}
@@ -68,25 +73,43 @@ const CartView = (props) => {
             animate="visibleB"
             exit="exitB"
         >
-            CART
+        <div className="nes-container-cart">
+            <div className="nes-container-cart-title">
+                CART
+            </div>
 
             {cartItems.map(cartItem => 
             <div key={cartItem._id}>
-                <Link to={'/products/' + cartItem._id}>
-                    <li key={cartItem._id}>{cartItem.name} : {cartItem.quantity}</li>
+            <div className="cart-grid">
+                <Link to={'/products/' + cartItem._id} className="cart-grid-image">
+                    <img key={cartItem._id} src={cartItem.image} alt="product"/>
                 </Link>
-
-                Qty: <select value={cartItem.quantity} onChange={(e) => dispatch(adjustQuantityCart(cartItem, e.target.value))}>
+                <Link to={'/products/' + cartItem._id} className="cart-grid-name">
+                    <li key={cartItem._id}>{cartItem.name}</li>
+                </Link>
+                <div className="cart-grid-qty">
+                <div className="qty-qty">Qty: &zwnj;</div> <select value={cartItem.quantity} onChange={(e) => dispatch(adjustQuantityCart(cartItem, e.target.value))}>
                         {[...Array(cartItem.numberInStock).keys()].map(x =>
                             <option key={x+1} value={x+1}>{x+1}</option>
                         )}
                        </select> 
 
-                    <button type="button" onClick={() => removeItemHandler(cartItem._id)}>Remove</button>
+                    <motion.button 
+                    type="button" 
+                    onClick={() => removeItemHandler(cartItem._id)}
+                    whileHover={{
+                        scale: 1.05,
+                        transition: { duration: .25 },
+                    }}
+                    >
+                    Remove
+                    </motion.button>
+                </div>
+            </div>
             <div className="cart-list-divider" />
             </div>
-            
             )}
+        </div>
         </motion.div>
 
         <motion.div className="cart-total"
@@ -97,11 +120,19 @@ const CartView = (props) => {
         >
             <div className="cart-total-title">Subtotal</div>
             <div className="cart-total-item">({totalItems} Items)</div>
-            <div className="cart-total-amount">${cartTotal}</div>
+            <div className="cart-total-amount"> <img className="rupee" src={rupee} alt="rupee" /> {cartTotal}</div>
             
 
             
-            <button onClick={checkoutHandler}>CHECKOUT</button>
+            <motion.button 
+            onClick={checkoutHandler}
+            whileHover={{
+                scale: 1.05,
+                transition: { duration: .25 },
+            }}
+            >
+            CHECKOUT
+            </motion.button>
         </motion.div>
 
         </div>
